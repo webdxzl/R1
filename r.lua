61273123c50979c8e4b9abbbbd4817829b807809
webdxzl <trixxywt@gmail.com>
2025-08-07 19:38:00 +0200

    Update r.lua

--- r.lua
+++ r.lua
@@ -1,4 +1,4 @@
-local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
+local WindUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Footagesus/WindUI/main/main.lua"))()
 
 task.wait(1)
 
